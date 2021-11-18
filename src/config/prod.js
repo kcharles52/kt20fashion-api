786e3f30e492fb345690590c4bf760c48186fb39
Kato Charles <kcharles52@gmail.com>
2021-11-18 22:20:37 +0300

    - fix port and db name

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,5 +2,5 @@ export const config = {
   secrets: {
     jwt: 'inspiredbythedesiretolearn',
   },
-  dbUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jxhcd.mongodb.net/saloon?retryWrites=true&w=majority`,
+  dbUrl: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jxhcd.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
 }
